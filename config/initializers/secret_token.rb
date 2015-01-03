296640d28adaacd9dc399a52748b1d4ebc3b1d24
Benjamin Crouzier <pinouchon@gmail.com>
2015-01-03 15:40:11 +0100

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vivi::Application.config.secret_key_base = '34b29639339617d17911c88825c002efb158b4b0c1b3b7885058403c61a8218eee2d95eebf6da4b24b37984b4c23f8da5ef2aa80e19d4684af259bb834aeae3a'
